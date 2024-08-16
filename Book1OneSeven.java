9d34c194896734c7da1de4a0e88ba1ca6d3c93e0
Joseph Aldape <153608483+MaxxOverload@users.noreply.github.com>
2024-08-16 19:48:36 -0400

    Added files via upload
    MTU CS1121 book Chapter 1 problem 7

---
+++ Book1OneSeven.java
@@ -0,0 +1,10 @@
+/*
+* Michigan Technological University CS1121 Problem 1-7
+* Approximating π
+*/
+public class Book1OneSeven {
+	public static void main(String[] args) {
+		System.out.print(4 * (1.0 - (1.0 / 3) + (1.0 / 5) - 
+			(1.0 / 7) + (1.0 / 9) - (1.0 / 11) + (1.0 / 13) ) );
+	}
+}
