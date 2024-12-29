d6d4208475b102e2cd7f1319b84b2bde0c8c3af0
Joseph Aldape <153608483+MaxxOverload@users.noreply.github.com>
2024-12-29 12:32:05 -0500

    Add files via upload
    Finding wind-chill temperature

---
+++ Book1TwoSeventeen.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/*
+* Michigan Technological University CS1121 Problem 2-17
+* Formula for wind chill
+*/
+public class Book1TwoSeventeen {
+	public static void main(String[] args) {
+		double outsideTemp = 0.0;
+		double windSpeed = 0.0;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter the temperature in Fahrenheit: ");
+		outsideTemp = input.nextDouble();
+		System.out.print("Enter the wind speed miles per hour: ");
+		windSpeed = input.nextDouble();
+		
+		double windChillTemp = 35.74 + (0.6215 * outsideTemp) - (35.75 * Math.pow(windSpeed, 0.16) )
+			+ (0.4275 * outsideTemp * Math.pow(windSpeed, 0.16) );
+		
+		System.out.print("The wind chill index is " + windChillTemp);
+	}
+	
+}
